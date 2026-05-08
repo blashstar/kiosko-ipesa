fdb216b082c63f64e6a0b3b114057e628fde2e65
Joffré Sánchez <blashstar@gmail.com>
2026-05-08 09:44:32 -0500

    chore: instalar vite-plugin-stylus-alias para alias en stylus

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import json5 from 'vite-plugin-json5'
+import stylusAliasModule from 'vite-plugin-stylus-alias'
 import { resolve } from 'path'
 
+const stylusAlias = (stylusAliasModule as any).default || stylusAliasModule;
+
 export default defineConfig({
-  plugins: [vue(), json5()],
+  plugins: [vue(), json5(), stylusAlias()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       '@estilos': resolve(__dirname, 'src/estilos'),
       '@utiles': resolve(__dirname, 'src/utiles'),
+      '@componentes': resolve(__dirname, 'src/componentes'),
+      '@comun': resolve(__dirname, 'src/componentes/comun'),
+      '@jd': resolve(__dirname, 'src/componentes/jd'),
     },
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     stylus: {},
-  //   },
-  // },
   base: './',
   build: {
     outDir: 'dist',
@@ -24,5 +25,16 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+  },
+
+  css: {
+    preprocessorOptions: {
+      stylus: {
+        imports: [
+          resolve(__dirname, 'src/estilos/capas/_variables.styl'),
+          resolve(__dirname, 'src/estilos/calculos.styl')
+        ],
+      },
+    },
   },
 })
