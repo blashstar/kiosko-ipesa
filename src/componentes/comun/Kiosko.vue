f606b899338c4a4b98f5f15b8d7d87235fa7ba6d
Joffré Sánchez <blashstar@gmail.com>
2026-05-08 09:47:52 -0500

    refactor: refactorizar Kiosko.vue y actualizar estilos de capas

--- src/componentes/comun/Kiosko.vue
+++ src/componentes/comun/Kiosko.vue
@@ -15,28 +15,28 @@ const kioskoElemento = ref<HTMLElement | null>(null)
 const claseBarras = ref<string>('')
 
 function alRedimensionar(entrada: ResizeObserverEntry) {
-  const { ancho, alto } = leerDimensiones(kioskoElemento.value);
-  const relacionAspecto = calcularRatio(ancho, alto);
+	const { ancho, alto } = leerDimensiones(kioskoElemento.value);
+	const relacionAspecto = calcularRatio(ancho, alto);
 
-  const { ancho:anchoArea, alto:altoArea } = extraerDimensiones(entrada, kioskoElemento);
-  const relacionArea = calcularRatio(anchoArea, altoArea);
+	const { ancho:anchoArea, alto:altoArea } = extraerDimensiones(entrada, kioskoElemento);
+	const relacionArea = calcularRatio(anchoArea, altoArea);
 
-  if (relacionArea < relacionAspecto) {
-	claseBarras.value = 'kiosko__aplicacion--hBarras';
-  } else if (relacionArea > relacionAspecto) {
-	claseBarras.value = 'kiosko__aplicacion--vBarras';
-  } else {
-	claseBarras.value = '';
-  }
+	if (relacionArea < relacionAspecto) {
+		claseBarras.value = 'kiosko__aplicacion--hBarras';
+	} else if (relacionArea > relacionAspecto) {
+		claseBarras.value = 'kiosko__aplicacion--vBarras';
+	} else {
+		claseBarras.value = '';
+	}
 
 }
 
 let detenerObserver: (() => void) | undefined
 
 onMounted(() => {
-  if (kioskoElemento.value) {
-    detenerObserver = usarObservadorRedimension(kioskoElemento.value, alRedimensionar)
-  }
+	if (kioskoElemento.value) {
+		detenerObserver = usarObservadorRedimension(kioskoElemento.value, alRedimensionar)
+	}
 })
 
 onBeforeUnmount(() => {
@@ -45,11 +45,8 @@ onBeforeUnmount(() => {
 </script>
 
 <style lang="stylus">
+
 .kiosko
-  --kiosko-escala            1
-  --kiosko-ancho             1080
-  --kiosko-alto              1920
-  --kiosko-relacion-aspecto  calc(var(--kiosko-ancho) / var(--kiosko-alto))
 
   // Dimensiones y layout principal
   width 100vw
