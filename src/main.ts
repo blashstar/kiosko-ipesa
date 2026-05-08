afa85a87c36b551a98a8a853312522086224490f
Joffré Sánchez <blashstar@gmail.com>
2026-05-08 09:46:34 -0500

    feat: incluir fuentes Gotham y configurar import en main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './estilos/globales.styl'
+import '@/estilos/globales.styl'
 
 const app = createApp(App)
 const pinia = createPinia()
