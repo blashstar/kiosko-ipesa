3ea6e32399e11eebb91c42598e7cd79e9f010e96
Joffré Sánchez <blashstar@gmail.com>
2026-05-11 08:14:05 -0500

    feat: agrega componentes JDLogoSeccion y JDPanel

---
+++ src/componentes/jd/JDLogoSeccion.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+figure.jd_logoSeccion
+    img(:src="logoActual")
+</template>
+
+
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  seccion: string;
+}>();
+
+const logos = {
+  'vistas-360': '/img/icono-vistas-360.webp',
+  'infografias': '/img/icono-infografia.webp',
+  'versus': '/img/icono-versus.webp'
+};
+
+const gif1px = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+
+const logoActual = computed(() => logos[props.seccion] || 'gif1px');
+
+
+</script>
+
+<style lang="stylus" scoped>
+
+.jd_logoSeccion
+	margin-inline vh(64px)
+	img
+		display block
+
+</style>
---
+++ src/componentes/jd/JDPanel.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+	.jd_panel
+		slot
+</template>
+
+
+
+<script setup lang="ts">
+</script>
+
+<style lang="stylus" scoped>
+
+.jd_panel
+	border-radius vh(24px)
+
+	background linear-gradient(135deg, rgba(64, 64, 64, 0.96), rgba(0, 0, 0, 0.96)) padding-box,
+				linear-gradient(135deg, #FFCC00, #FCB515) border-box
+	background-clip padding-box, border-box
+	border 1px solid transparent
+
+	box-shadow 6px 7px 5.2px rgba(0, 0, 0, 0.48)
+	box-sizing border-box
+	position relative
+	margin 8px
+	padding 16px
+</style>
